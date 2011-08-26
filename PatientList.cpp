ffc356cc8c3c154a09d1b60a3c3aa38c75740f2d
Michał Łuszczyk <michal@agmarml.pl>
2011-08-26 15:22:48 +0200

    headers labels in PatientList

--- PatientList.cpp
+++ PatientList.cpp
@@ -10,7 +10,7 @@ namespace Fizjoterapia {
 
 PatientList::PatientList() {
 	layout = new QVBoxLayout(this);
-	model = new QStandardItemModel;
+	model = new QStandardItemModel(7, 0);
 	view = new QTableView;
 
 	view->setModel(model);
@@ -40,6 +40,16 @@ void PatientList::refill() {
 			row.append(new QStandardItem(q.value(i).toString()));
 		model->appendRow(row);
 	}
+	model->setHeaderData(0, Qt::Horizontal, "ID");
+	model->setHeaderData(1, Qt::Horizontal, QString::fromUtf8("Imię"));
+	model->setHeaderData(2, Qt::Horizontal, "Nazwisko");
+	model->setHeaderData(3, Qt::Horizontal, QString::fromUtf8("Płeć"));
+	model->setHeaderData(4, Qt::Horizontal, "Data urodzenia");
+	model->setHeaderData(5, Qt::Horizontal, "Pierwsza wizyta");
+	model->setHeaderData(6, Qt::Horizontal, "Ostatnia wizyta");
+
+	view->setColumnHidden(0, true);
+	view->resizeColumnsToContents();
 }
 
 void PatientList::selectedPatient(const QItemSelection &current,
